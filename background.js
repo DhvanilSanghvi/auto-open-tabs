492efd97841c1e44cd98ddf82977189a62e7f475
DhvanilSanghvi <39693265+DhvanilSanghvi@users.noreply.github.com>
2020-08-04 17:44:08 +0530

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({url:"https://mail.google.com/mail/u/0/"});
-});
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({url:"https://mail.google.com/mail/u/1/"});
-});
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({url:"https://kite.zerodha.com/"});
-});
